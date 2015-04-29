6ef317e36bb7e9915da65d8a7df7e3df8ccb0567
corvusnigra <itemruby@gmail.com>
2015-04-29 19:43:22 +0400

    Added browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    browserify = require('gulp-browserify');
 
     
     var coffeeSources = ['components/coffee/*'];
@@ -10,6 +11,7 @@ var gulp = require('gulp'),
     gulp.task('js', function() {
         gulp.src(jsSources)
         .pipe(concat('script.js'))
+        .pipe(browserify())
         .pipe(gulp.dest('builds/development/js'))
     });
 
