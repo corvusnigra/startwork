18d8c846fd1a12c2782b9440d3f21fbbb8883265
corvusnigra <itemruby@gmail.com>
2015-04-29 19:25:17 +0400

    Added coffeescript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee');
 
-
+    
+    var coffeeSources = ['components/coffee/*']
     gulp.task('log', function() {
     	gutil.log('work')
+    });
+
+
+    gulp.task('coffee', function() {
+    	gulp.src(coffeeSources)
+    	.pipe(coffee({bare : true})
+    	.on('error', gutil.log))
+    	.pipe(gulp.dest('components/scripts'))
     });
 
 
